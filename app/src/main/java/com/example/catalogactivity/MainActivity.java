30555797d763c72fc8d7d1ac4f840cc6b8925b59
Eduardo Pablo Lopez Sanchez <elopezs23@fpcoruna.afundacion.org>
2025-11-10 11:55:11 +0100

    Tarea 2, Empty Views Activity llamada DetailActivity

--- app/src/main/java/com/example/catalogactivity/MainActivity.java
+++ app/src/main/java/com/example/catalogactivity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.catalogactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button navigateButton = findViewById(R.id.button_navigate);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
